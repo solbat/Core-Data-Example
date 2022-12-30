02a3540d2189b6f5e8228b973a2146e6d406fea3
kimminjun9 <kimminjun9@worksmobile.com>
2022-12-30 18:29:09 +0900

    [chore] 프로젝트 생성

---
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by USER on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
