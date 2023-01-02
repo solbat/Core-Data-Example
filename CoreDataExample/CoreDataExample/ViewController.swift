969da9b77dae7cf2547eb9b7472f792505a6566d
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-02 16:07:12 +0900

    [feat] viewDidLoad

--- CoreDataExample/CoreDataExample/ViewController.swift
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -21,7 +21,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureView()
+        configure()
+        getAllItems()
     }
     
     func configureView() {
