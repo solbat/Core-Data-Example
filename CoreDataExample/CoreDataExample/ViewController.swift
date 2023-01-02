5c9b8d8f838c3fd6d46363ddc53000836ade3f22
kimminjun9 <kimminjun9@worksmobile.com>
2023-01-02 16:00:32 +0900

    [feat] TableView 관련 설정

--- CoreDataExample/CoreDataExample/ViewController.swift
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -28,6 +28,12 @@ class ViewController: UIViewController {
                                                             target: self,
                                                             action: #selector(didTapAdd))
     }
+    
+    func configure() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
         
     }
     
@@ -44,3 +50,45 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    // TableView의 셀 개수
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    // TableView 셀 세팅
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.name
+        return cell
+    }
+    
+    // TableView 셀 선택 시
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        
+        // actionSheet
+        let sheet = UIAlertController(title: "Edit",
+                                      message: nil,
+                                      preferredStyle: .actionSheet)
+        
+        // Edit
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            let alert = UIAlertController(title: "Edit Item",
+                                          message: "Edit your item",
+                                          preferredStyle: .alert)
+            
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+            }))
+            
+            self.present(alert, animated: true)
+        }))
